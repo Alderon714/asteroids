566c205e0b11b14d829ac6798e2ce6771238f6da
Alderon714 <dcochran@coastal.edu>
2024-11-19 16:58:20 -0500

    Sprites

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-# this allows us to use code from 
-# open-source pygame library
-# throughout this file
+# this allows us to use code from open-source pygame library throughout this file
 #
 # Daniel S Cochran
-# Never 19, 2024
+# November 19, 2024
 #
 import pygame
 from constants import * # import everything from the constants.py file/module
@@ -14,13 +12,18 @@ def main():
     # print(f"Screen height:",SCREEN_HEIGHT)
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
+    clock = pygame.time.Clock() # allows for milosecond pause in CPU usage.
+    dt = 0 # Delta Time
+    while True: # infinate loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick() / 1000
+        # print("delta time:", dt) # that worked *rolls eyes* 
 
 if __name__ == "__main__":
     main()
